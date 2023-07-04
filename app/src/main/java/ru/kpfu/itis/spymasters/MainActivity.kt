7428dc9f5f1fc5f9287134473e474ecc6e1b77ac
foreverestBulat <113981993+foreverestBulat@users.noreply.github.com>
2023-07-04 11:19:11 +0300

    try

--- app/src/main/java/ru/kpfu/itis/spymasters/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/spymasters/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.kpfu.itis.spymasters
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
